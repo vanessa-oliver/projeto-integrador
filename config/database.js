5fd810333e051327e18156eea8c30b98bebcdfc0
vanessa-oliver <vanessa.n.d.oliveira@gmail.com>
2025-01-29 11:26:38 -0300

    Update database.js for use postgres

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mysql',
+        dialect: 'postgres',
     }
 );
 sequelize.authenticate() 
